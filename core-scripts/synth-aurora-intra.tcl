f8c202abd8807f2022b337938c5ddb54212a9467
Chanwoo Chung <cwchung@mit.edu>
2015-11-20 17:36:57 -0500

    v15.07.3 - remote/multi compile

--- core-scripts/synth-aurora-intra.tcl
+++ core-scripts/synth-aurora-intra.tcl
@@ -6,5 +6,8 @@ set core_version "10.2"
 if {[version -short] >= "2014.4"} {
     set core_version "10.3"
 }
+if {[version -short] >= "2015.1"} {
+    set core_version "11.0"
+}
 
-connectal_synth_ip aurora_8b10b 10.3 aurora_8b10b_fmc1 [list CONFIG.C_AURORA_LANES {4} CONFIG.C_LANE_WIDTH {4} CONFIG.C_LINE_RATE {4.4} CONFIG.C_REFCLK_FREQUENCY {275.000} CONFIG.Interface_Mode {Streaming} CONFIG.C_GT_LOC_24 {4} CONFIG.C_GT_LOC_23 {3} CONFIG.C_GT_LOC_22 {2} CONFIG.C_GT_LOC_21 {1} CONFIG.C_GT_LOC_1 {X}]
+connectal_synth_ip aurora_8b10b $core_version aurora_8b10b_fmc1 [list CONFIG.C_AURORA_LANES {4} CONFIG.C_LANE_WIDTH {4} CONFIG.C_LINE_RATE {4.4} CONFIG.C_REFCLK_FREQUENCY {275.000} CONFIG.Interface_Mode {Streaming} CONFIG.C_GT_LOC_24 {4} CONFIG.C_GT_LOC_23 {3} CONFIG.C_GT_LOC_22 {2} CONFIG.C_GT_LOC_21 {1} CONFIG.C_GT_LOC_1 {X}]
--- init_xbsv.sh
+++ init_xbsv.sh
@@ -7,11 +7,12 @@ cd tools;
 git clone https://github.com/cambridgehackers/connectal.git connectal
 cd connectal;
 #git reset --hard 62a9af99ed464ee88c99292f661cc9f31a3a5371
-git reset --hard v15.06.3
+git reset --hard v15.07.3
 cd ../;
 
 git clone https://github.com/cambridgehackers/fpgamake.git
 cd fpgamake;
+git pull;
 #git reset --hard 7cbe96eb18b72e99ff71cb34f361c85cfc375074
 #git reset --hard fc6a8bc9357c23a94a79cf1133cc878d2d77d3c6
 cd ../;
@@ -19,6 +20,7 @@ cd ../;
 
 git clone https://github.com/cambridgehackers/buildcache.git
 cd buildcache;
+git pull;
 #git reset --hard 029ebfad1db0394c28cd3830ba13badf2b6750d9
 #git reset --hard 07c4aab5c1389b577d260ab6da2ca79240b61c24
 cd ../;
